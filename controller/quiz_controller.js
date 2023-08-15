85cbb5c986148a143f411fbe626fc173ef0e3cc2
Shashank <shashanksinghal02@gmail.com>
2023-08-16 03:55:39 +0530

    - Response modification in quiz list

--- controller/quiz_controller.js
+++ controller/quiz_controller.js
@@ -10,18 +10,18 @@ const createQuiz = asyncHandler(async (req, res) => {
         // Check if a quiz with the same title already exists
         const existingQuiz = await Quiz.findOne({ title: title });
         if (existingQuiz) {
-            return res.status(401).json({ message: 'Quiz with this title already exists' });
+            return res.json({ code: 401, status: false, message: 'Quiz with this title already exists' });
         }
 
         // Check if the provided question_category_id is a valid ObjectId
         if (!validateMongoDbId(category)) {
-            return res.status(400).json({ message: 'Invalid quiz category id format' });
+            return res.json({ code: 400, status: false, message: 'Invalid quiz category id format' });
         }
 
         // Create a new quiz using the Quiz model
         const newQuiz = await Quiz.create(req.body);
 
-        res.status(201).json(newQuiz); // Return the created quiz as the response.
+        res.json({ code: 200, status: true, newQuiz: newQuiz }); // Return the created quiz as the response.
     } catch (err) {
         throw new Error(err);
     }
@@ -34,11 +34,12 @@ const getAllQuiz = asyncHandler(async (req, res) => {
         const quizCount = await Quiz.countDocuments();
         if (allQuizzes.length > 0) {
             res.json({
+                code: 200, status: true,
                 count: quizCount,
                 quizzes: allQuizzes,
             });
         } else {
-            res.status(404).json({ message: 'No quizzes found' });
+            res.json({ code: 404, status: false, message: 'No quizzes found' });
         }
     }
     catch (err) {
@@ -53,14 +54,14 @@ const getSpecificQuiz = asyncHandler(async (req, res) => {
 
         // Check if the provided quiz_id is a valid ObjectId
         if (!validateMongoDbId(quiz_id)) {
-            return res.status(400).json({ message: 'Invalid quiz_id format' });
+            return res.json({ code: 400, status: false, message: 'Invalid quiz_id format' });
         }
 
         const quiz = await Quiz.findById(quiz_id);
         if (quiz) {
-            res.json(quiz);
+            res.json({ code: 200, status: true, quiz: quiz });
         } else {
-            res.status(404).json({ message: 'Quiz not found' });
+            res.json({ code: 404, status: false, message: 'Quiz not found' });
         }
     } catch (err) {
         throw new Error(err);
@@ -74,16 +75,17 @@ const deleteSpecificQuiz = asyncHandler(async (req, res) => {
 
         // Check if the provided quiz_id is a valid ObjectId
         if (!validateMongoDbId(quiz_id)) {
-            return res.status(400).json({ message: 'Invalid quiz_id format' });
+            return res.json({ code: 400, status: false, message: 'Invalid quiz_id format' });
         }
 
         const deleteQuiz = await Quiz.findByIdAndDelete(quiz_id);
         if (deleteQuiz) {
             res.json({
+                code: 200, status: true,
                 message: 'Quiz deleted successfully'
             });
         } else {
-            res.status(404).json({ message: 'Quiz not found' });
+            res.json({ code: 404, status: false, message: 'Quiz not found' });
         }
     } catch (err) {
         throw new Error(err);
@@ -98,20 +100,20 @@ const updateQuiz = asyncHandler(async (req, res) => {
 
         // Check if the provided quiz_id is a valid ObjectId
         if (!validateMongoDbId(quiz_id)) {
-            return res.status(400).json({ message: 'Invalid quiz_id format' });
+            return res.json({ code: 400, status: false, message: 'Invalid quiz_id format' });
         }
 
         // Find the quiz by ID
         const quiz = await Quiz.findById(quiz_id);
         if (!quiz) {
-            return res.status(404).json({ message: 'Quiz not found' });
+            return res.json({ code: 404, status: false, message: 'Quiz not found' });
         }
 
         // Check if the updated title already exists for another quiz
         if (title !== quiz.title) {
             const existingQuiz = await Quiz.findOne({ title: title });
             if (existingQuiz) {
-                return res.status(409).json({ message: 'Quiz with this title already exists' });
+                return res.json({ code: 409, status: false, message: 'Quiz with this title already exists' });
             }
         }
 
@@ -127,7 +129,7 @@ const updateQuiz = asyncHandler(async (req, res) => {
             }
         );
 
-        res.json(updatedQuiz);
+        res.json({ code: 200, status: true, updatedQuiz: updatedQuiz });
     } catch (err) {
         throw new Error(err);
     }
